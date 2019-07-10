3ec28f2bdfdce288bb1df27fc343ce3fc7c272d2
anas <chopa1939@gmail.com>
2019-07-10 20:20:48 +0300

    copleted sticky header functionality.

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,7 +1,9 @@
 import mobiolemenu from './modules/mobilemenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 
 var mobilemenu = new mobiolemenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
+var stickyHeader = new StickyHeader();
